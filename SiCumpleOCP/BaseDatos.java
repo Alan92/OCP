a3b095785c05c3025264af7f720fd485b29059b7
Alan92 <wensescalo4@gmail.com>
2013-03-31 20:28:44 +0200

    Update BaseDatos.java

--- SiCumpleOCP/BaseDatos.java
+++ SiCumpleOCP/BaseDatos.java
@@ -1,5 +1,5 @@
-class DataBaseProvider implements UserProvider{
-    public List getUsers() {
+class BaseDatos implements Proveedor {
+    public List obtenerUsuarios() {
         // Aqui nuestro codigo q busca usuarios en la Base de Datos
         return new ArrayList();
     }
