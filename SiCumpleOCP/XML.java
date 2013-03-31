d3ae3b29bbe4b9d66adbc15b019371d7f69a586f
Alan92 <wensescalo4@gmail.com>
2013-03-31 20:30:00 +0200

    Update XML.java

--- SiCumpleOCP/XML.java
+++ SiCumpleOCP/XML.java
@@ -1,5 +1,5 @@
-class XmlProvider implements UserProvider{
-    public List getUsers() {
+class XML implements Proveedor {
+    public List obtenerUsuarios() {
         //Aqui nuestro codigo q busca usuarios en un XML
         return new ArrayList();
     }
